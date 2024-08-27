a6d30abba4508608538ea01d9ed9f68e91553ec8
root <root@SAMSUNG>
2024-08-27 14:38:27 +0300

    Create a count_calls decorater and wrap store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,14 +2,24 @@
 """ Create a cache instance """
 import redis
 import uuid
+from functools import wraps
 from typing import Callable, Optional, Union
 
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
